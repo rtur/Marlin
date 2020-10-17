392f6481c645319823b56c479d33fc241cb83226
rtur <rtur@rtur.org>
2020-10-17 11:07:29 +0200

    use a hybrid threshold for stealthchop

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2469,24 +2469,24 @@
    * STEALTHCHOP_(XY|Z|E) must be enabled to use HYBRID_THRESHOLD.
    * M913 X/Y/Z/E to live tune the setting
    */
-  //#define HYBRID_THRESHOLD
+  #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
-  #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     100
-  #define Y2_HYBRID_THRESHOLD    100
+  #define X_HYBRID_THRESHOLD      50  // [mm/s]
+  #define X2_HYBRID_THRESHOLD     50
+  #define Y_HYBRID_THRESHOLD      50
+  #define Y2_HYBRID_THRESHOLD     50
   #define Z_HYBRID_THRESHOLD       3
   #define Z2_HYBRID_THRESHOLD      3
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
-  #define E0_HYBRID_THRESHOLD     30
-  #define E1_HYBRID_THRESHOLD     30
-  #define E2_HYBRID_THRESHOLD     30
-  #define E3_HYBRID_THRESHOLD     30
-  #define E4_HYBRID_THRESHOLD     30
-  #define E5_HYBRID_THRESHOLD     30
-  #define E6_HYBRID_THRESHOLD     30
-  #define E7_HYBRID_THRESHOLD     30
+  #define E0_HYBRID_THRESHOLD     20
+  #define E1_HYBRID_THRESHOLD     20
+  #define E2_HYBRID_THRESHOLD     20
+  #define E3_HYBRID_THRESHOLD     20
+  #define E4_HYBRID_THRESHOLD     20
+  #define E5_HYBRID_THRESHOLD     20
+  #define E6_HYBRID_THRESHOLD     20
+  #define E7_HYBRID_THRESHOLD     20
 
   /**
    * Use StallGuard to home / probe X, Y, Z.
